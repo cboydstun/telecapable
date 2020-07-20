d9aeb77f3f220fa43379ffcf12ff1b89a57d4c9c
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2020-07-19 19:15:08 -0500

    init commit
    with NavBar

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+import NavBar from './components/NavBar'
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <h1>hello world</h1>
+    </div>
+  );
+}
+
+export default App;
