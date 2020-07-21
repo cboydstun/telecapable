a2e1b72d019c190412a30811d47becc05e891801
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2020-07-21 03:08:08 -0500

    added top carousel

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 
-import NavBar from './components/NavBar'
+import NavBar from './components/NavBar/NavBar'
+import Carousel from './components/Carousel/Carousel'
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Carousel />
       <h1>hello world</h1>
     </div>
   );
